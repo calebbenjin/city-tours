54d70dd9eb1f2779321d34c04518a1cf374945c1
calebbenjin <calebben94@gmail.com>
2021-02-19 16:50:27 +0100

    city tours display

--- src/App.js
+++ src/App.js
@@ -4,8 +4,60 @@ import Tours from './Tours'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
+
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [IsLoading, setIsLoading] = useState(true)
+  const [tours, setTours] = useState([])
+
+  const removeTour = (id) => {
+    const newTour = tours.filter((tour) => tour.id !== id)
+    setTours(newTour)
+  }
+
+  const fetchTours = async () => {
+    setIsLoading(true)
+
+    try {
+      const response = await fetch(url)
+      const tours = await response.json()
+      setIsLoading(false)
+      setTours(tours)
+    } catch (error) {
+      setIsLoading(false)
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchTours()
+  }, [])
+
+  if (IsLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>no tours left</h2>
+          <button className='btn' onClick={fetchTours}>
+            refresh
+          </button>
+        </div>
+      </main>
+    )
+  }
+
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  )
 }
 
 export default App
